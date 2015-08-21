d5d9e8ba55e6143f66151022f5853f319ed7780e
marb <marb61a@gmail.com>
2015-08-21 13:22:39 +0000

    updated script.js file

--- js/script.js
+++ js/script.js
@@ -27,4 +27,44 @@ $(document).ready(function(){
 		// Stop the link behaviour
 		return false;
 	});
+	
+	// Mouseenter Overlay Effect
+	$('ul#gallery li').on('mouseenter',function(){
+		// Get data attribute values
+		var title = $(this).children().data('title');
+		var desc = $(this).children().data('desc');
+
+		if(desc == null){
+			desc = 'Click To Enlarge';
+		}
+
+		if(title == null){
+			title = '';
+		}
+
+		// Create an overlay div
+		$(this).append('<div class="overlay"></div>');
+
+		// Get the overlay div
+		var overlay = $(this).children('.overlay');
+
+		// Add html to overlay
+		overlay.html('<h3>'+title+'</h3><p>'+desc+'</p>');
+
+		// Fade in overlay
+		overlay.fadeIn(800);
+	});
+
+	// Mouseleave Overlay Effect
+	$('ul#gallery li').on('mouseleave',function(){
+		// Create an overlay div
+		$(this).append('<div class="overlay"></div>');
+
+		// Get the overlay div
+		var overlay = $(this).children('.overlay');
+
+		// Fade out overlay
+		overlay.fadeOut(500);
+	});
+
 });
